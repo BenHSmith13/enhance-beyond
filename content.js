1d74b51f71c0f059bf39cea11a8ca35528dd5785
Ben Smith <benhsmith13@gmail.com>
2019-01-14 20:06:40 -0700

    gives the sidebar the power slide

--- content.js
+++ content.js
@@ -2,6 +2,8 @@ const sidebarWidth = 256;
 
 // Create the sidebar element
 const content = document.createElement('div');
+
+content.id = 'sidebar-container';
 content.innerHTML = `
   <div id="enhance-beyond"></div>
 `;
@@ -11,11 +13,38 @@ content.style = `
   position: fixed;
   right: 0px;
   top: 0px;
+  transition: ease .5s all;
 `;
 document.body.appendChild(content);
 
+// adds the toggle button
+// do we want a button to toggle open or do we want it to just open when you mouse near it and stay open intill you leave?
+const button = document.createElement('div');
+button.id = 'sidebar-toggle';
+button.innerHTML = 'Cool Logo Here'
+button.style = `
+  position: fixed;
+  top: 10px;
+  right: ${sidebarWidth}px;
+  background-color: purple;
+  curser: pointer;
+  transition: ease .5s all;
+`;
+button.onclick = () => {
+  const sidebar = document.getElementById('sidebar-container');
+  const button = document.getElementById('sidebar-toggle');
+  if (sidebar.style.right === '0px') {
+    sidebar.style.right = `-${sidebarWidth}px`;
+    button.style.right = `0px`;
+  } else {
+    sidebar.style.right = `0px`;
+    button.style.right = `${sidebarWidth}px`;
+  }
+}
+document.body.appendChild(button);
+
 // make room for the sidebar on the page
-document.body.style.width = document.body.clientwidth - sidebarWidth; // TODO: this doesn't work
+// document.body.style.width = document.body.clientwidth - sidebarWidth; // TODO: this doesn't work
 
 // Tell the backgound page to run the react code
 chrome.runtime.sendMessage("INJECT_REACT_APP", function(response) {
